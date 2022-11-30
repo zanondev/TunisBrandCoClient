6efd033a725c285eeadb6fd446d19e83acc081dc
Lucas <szanonn@gmail.com>
2022-11-29 21:48:45 -0300

    criado componentes features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientFormComponent } from './features/clients/client-form/client-form.component';
+import { ClientTableComponent } from './features/clients/client-table/client-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'clients',
+    children: [
+      {
+        path: 'form',
+        component: ClientFormComponent,
+      },
+      {
+        path: 'table',
+        component: ClientTableComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/template/header/header.component';
-import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { ClientTableComponent } from './features/clients/client-table/client-table.component';
+import { ClientFormComponent } from './features/clients/client-form/client-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ClientTableComponent,
+    ClientFormComponent
   ],
   imports: [
     BrowserModule,
