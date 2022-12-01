61f5fc3acfa54769a4d33962042cdecd48a4f279
Lucas <szanonn@gmail.com>
2022-12-01 13:29:48 -0300

    adicionado form incompleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
 import { ClientTableComponent } from './features/clients/client-table/client-table.component';
 import { ClientFormComponent } from './features/clients/client-form/client-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ClientService } from './features/clients/client.service';
 
@@ -23,6 +23,7 @@ import { ClientService } from './features/clients/client.service';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
   ],
   providers: [ClientService],
--- src/app/features/clients/client-form/client-form.component.ts
+++ src/app/features/clients/client-form/client-form.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { take } from 'rxjs';
+import { IClient } from '../client.model';
+import { ClientService } from '../client.service';
 
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
-  styleUrls: ['./client-form.component.css']
+  styleUrls: ['./client-form.component.css'],
 })
-export class ClientFormComponent {
+export class ClientFormComponent implements OnInit {
+  public form!: FormGroup;
 
+  constructor(private service: ClientService) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      cpf: new FormControl(null, [Validators.required, Validators.min(11)]),
+      birthdate: new FormControl(null, [Validators.required]),
+    });
+  }
+  public saveForm(): void {
+    if (this.form.valid) {
+      const novoCliente: IClient = {
+        id: 0,
+        name: this.form.get('name')?.value,
+        cpf: this.form.get('cpf')?.value,
+        birthDate: this.form.get('birthDate')?.value,
+        loyaltyPoints: 0,
+      };
+
+      this.service
+        .addClient(novoCliente)
+        .pipe(take(1))
+        .subscribe(() => {
+          alert('Cliente cadastrado com sucesso!');
+        });
+    }
+  }
+
+  public cleanForm() {
+    this.form.reset();
+  }
 }
